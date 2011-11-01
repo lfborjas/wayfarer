7f1f38b7cab8bda021563f13d7c4f98e681c088c
lfborjas <luis.borjas@escolarea.com>
2011-10-31 22:06:37 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wayfarer::Application.config.secret_token = 'c9d3a7a5f720772e38f85a4372a24bb77fc1db4185827aac4c17588ee05f7f1ca1250fd4746f34c129fb49105611f7b8a283759b70d6e5e56c0db391b1bf6222'
