d5809e9e6f8aea739a8d5ec83de7b58a6074bb77
lfborjas <luis.borjas@escolarea.com>
2011-11-09 05:07:03 -0600

    beginning the embed thingy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 class HomeController < ApplicationController
   def index
+    respond_to do |format|
+      @mode = params[:mode] || 'full'
+      @width = params[:width] || 500
+      @height = params[:height] || 500
+      format.js {render :template => 'home/embed' ,:layout => false}
+      format.html do
+        template = if @mode.eql?('full')
+                     'home/index'
+                   else
+                     'home/small_embed'
+                   end
+        render :template => template
+      end
+    end
   end
-
 end
