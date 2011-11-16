ffd7db870bca30691511efbd5220339cbecb44f1
lfborjas <luis.borjas@escolarea.com>
2011-11-16 02:02:57 -0600

    creates templates for the gallery and adds a helper to render them

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,8 @@
 module HomeHelper
+  def render_jst
+    Dir.entries("#{Rails.root}/app/views/home/jst").select{|s| s["erb"]}.reduce("") do |result, name|
+      name =~ /_(\w+).*/
+      result + render("home/jst/#{$1}")
+    end
+  end
 end
