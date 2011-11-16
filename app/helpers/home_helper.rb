ddebe3f45cd51e902428e1aaced7f0a854d78109
lfborjas <luis.borjas@escolarea.com>
2011-11-16 03:06:51 -0600

    fancy-ass jst loading

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,7 +2,11 @@ module HomeHelper
   def render_jst
     Dir.entries("#{Rails.root}/app/views/home/jst").select{|s| s["erb"]}.reduce("") do |result, name|
       name =~ /_(\w+).*/
-      result + render("home/jst/#{$1}")
+      result + %Q|
+        <script type="text/template" id="#{$1}-template">
+          #{render("home/jst/#{$1}")}
+        </script>
+      |
     end
   end
 end
